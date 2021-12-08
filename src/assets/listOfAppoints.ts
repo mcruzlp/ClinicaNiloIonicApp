23c582e45c36e16aeeb396a538910caca1cc08b8
mcruzlp <mcruzlp@gmail.com>
2021-12-08 17:02:40 +0100

    citas

--- src/assets/listOfAppoints.ts
+++ src/assets/listOfAppoints.ts
@@ -1,5 +1,29 @@
 import { Appointment } from "src/app/model/appointment";
 
 export const LIST_OF_APPOINTMTS: Appointment[] = [
-
+  {id: 0, date:'10/01/2022', hour:'09:00', patient:'Trudi Lazell'},
+  {id: 1, date:'10/01/2022', hour:'10:00', patient:'Nickie Hoble'},
+  {id: 2, date:'10/01/2022', hour:'11:00', patient:'Saudra Nickoles'},
+  {id: 3, date:'10/01/2022', hour:'12:00', patient:'Bogart Lucian'},
+  {id: 4, date:'11/01/2022', hour:'09:00', patient:'Gizela De Domenicis'},
+  {id: 5, date:'11/01/2022', hour:'10:00', patient:'Waylon Patrie'},
+  {id: 6, date:'11/01/2022', hour:'11:00', patient:'Mattie Clapham'},
+  {id: 7, date:'11/01/2022', hour:'12:00', patient:'Jemima Fessler'},
+  {id: 8, date:'12/01/2022', hour:'09:00', patient:'Cassandre McNabb'},
+  {id: 9, date:'12/01/2022', hour:'10:00', patient:'Glendon Wade'},
+  {id: 10, date:'12/01/2022', hour:'11:00', patient:'Imojean Helis'},
+  {id: 11, date:'12/01/2022', hour:'12:00', patient:'Ardene Bricket'},
+  {id: 12, date:'13/01/2022', hour:'09:00', patient:'Jesse Kingswood'},
+  {id: 13, date:'13/01/2022', hour:'10:00', patient:'Emmye Englishby'},
+  {id: 14, date:'13/01/2022', hour:'11:00', patient:'Veronica Boneham'},
+  {id: 15, date:'13/01/2022', hour:'12:00', patient:'Rora Lafford'},
+  {id: 16, date:'14/01/2022', hour:'09:00', patient:'Laurent Dusting'},
+  {id: 17, date:'14/01/2022', hour:'10:00', patient:'Sebastian Bladen'},
+  {id: 18, date:'14/01/2022', hour:'11:00', patient:'Nanny Pizzey'},
+  {id: 19, date:'14/01/2022', hour:'12:00', patient:'Antony Dacke'},
+  {id: 20, date:'17/01/2022', hour:'09:00', patient:'Addia Dysart'},
+  {id: 21, date:'17/01/2022', hour:'10:00', patient:'Anallise Cone'},
+  {id: 22, date:'17/01/2022', hour:'11:00', patient:'Carmel Feely'},
+  {id: 23, date:'17/01/2022', hour:'12:00', patient:'Robbert Boseley'},
+  {id: 24, date:'17/01/2022', hour:'13:00', patient:'Warner Ropert'}
 ];
